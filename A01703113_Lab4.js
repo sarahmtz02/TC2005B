db907cc7ade181bb10528d0a49e9329b74f5c8c0
Sarah M N <sarahmn@MacBook-Air-de-Carlos.local>
2022-02-20 22:10:55 -0600

    INIT LAB5

--- A01703113_Lab4.js
+++ A01703113_Lab4.js
@@ -1,2 +1,23 @@
-//hellouuu
-//i wanna sleep XD
+//1
+const num = prompt("Ingrese un número: ");
+console.log(num);
+for (let i = 1; i<=num; i++){
+    console.log(i*i);
+    console.log(i*i*i);
+    i++;
+}
+
+//2
+
+
+//3
+
+
+//4
+
+
+//5
+
+
+//6
+
