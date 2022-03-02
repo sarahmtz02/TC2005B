13427fafb92e50b5b13c6ed7384d0d30a6d19324
Sarah M N <sarahmn@MacBook-Air-de-Carlos.local>
2022-03-01 23:58:17 -0600

    Apagando compu...

--- A01703113_LAB6.js
+++ A01703113_LAB6.js
@@ -1,1 +1,14 @@
 //JAVASCRIPT LAB 6
+
+function validar(){
+    let txt_confirm=document.getElementById("txt_confirm").value;
+    let txt_contraseña=document.getElementById("txt_contraseña").value;
+
+    if(txt_contraseña=="" & txt_confirm==""){
+        window.alert("No has ingresado tu contraseña");
+    }
+
+    else if(txt_contraseña=="" || txt_confirm==""){
+        
+    }
+}
