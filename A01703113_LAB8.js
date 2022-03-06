40d81500de99891cad02d8e42340c1d84cb48cfc
Sarah M N <sarahmn@MacBook-Air-de-Carlos.local>
2022-03-06 11:10:38 -0600

    LAB8 JS ERROR404

--- A01703113_LAB8.js
+++ A01703113_LAB8.js
@@ -3,4 +3,26 @@
 
 const http = require('http');
 
-listen.server(3000);
+const server = http.createServer( (request, response) => {
+
+    if (request.url === '/') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html><html lang="es-mx"><head><title>LAB 8</title><meta charset="utf-8"></meta></head>');
+        response.write('<body>');
+        response.write('<h1>Hellouuu desde el servidor local</h1>');
+        response.write('<main>');
+        response.write('<h2>URL PARA LOS LABS: </h2>');
+        response.write('<a target="_blank" href="LABS"> https:/Users/sarahmn/Desktop/Uni/Semestre 4/Construcción de Software 1-2/Labs/TC2005B/A01703113_LAB1.html</a>');
+    } 
+    else {
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html><html lang="es-mx"><head><title>ERROR | Page not found</title><meta charset="utf-8"></meta></head>');
+        response.write('<body>');
+        response.write('<h1>La página '+ request.url +' no se encontró</h1>');
+        response.write('</body>');
+        response.end();
+    }
+});
+
+server.listen(3000);
