d54d7e55333bb358680fba4dbb4f20cd31749a7c
sarahmtz02 <A01703113@tec.mx>
2022-05-04 13:39:09 -0500

    LAB14 userController

---
+++ controllers/users_controller.js
@@ -0,0 +1,25 @@
+
+const Usuario = require('../models/LAB14');
+
+exports.get_login = (request, response, next) => {
+    const usuario = request.session.usuario ? request.session.usuario: '';
+    console.log(request.session.usuario);
+    response.render('login', {
+        usuario: usuario
+    });
+};
+
+exports.login = (request, response, next) => {
+    if(Usuario.login(request.body.nombre,request.body.passwd)){
+        request.session.usuario = request.body.nombre;
+        response.redirect('/panques/');
+    } else{
+        response.redirect('/users/login');
+    }
+};
+
+exports.logout = (request,response,next) =>{
+    request.session.destroy(()=>{
+        response.redirect('/users/login')
+    });
+};
